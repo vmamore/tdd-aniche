0f7e16c429b3c1c59a6bdede49d595d6a492481a
unknown <vinicius.mamore@gmail.com>
2020-05-19 20:14:31 -0400

    adicionado tabela de de -> para numeros romanos

--- 3-introducao-ao-tdd/ConversorDeNumerosRomanosTestes.cs
+++ 3-introducao-ao-tdd/ConversorDeNumerosRomanosTestes.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using Xunit;
 
 namespace introducao_ao_tdd
@@ -23,11 +24,17 @@ namespace introducao_ao_tdd
     }
 
     public class ConversorDeNumeroRomano {
+        private Dictionary<string, int> tabela = new Dictionary<string, int>(){
+            {"I", 1},
+            {"V", 5},
+            {"X", 10},
+            {"L", 50},
+            {"C", 100},
+            {"D", 500},
+            {"M", 1000}
+        };
         public int Converte(string numeroEmRomano){
-            if(numeroEmRomano.Equals("I")) return 1;
-            else if(numeroEmRomano.Equals("V")) return 5;
-
-            return 0;
+            return tabela[numeroEmRomano];
         }
     }
 }
