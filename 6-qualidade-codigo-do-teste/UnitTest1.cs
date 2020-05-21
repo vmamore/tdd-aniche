c4890054d0b4718f1fdfcb920399807309c18112
unknown <vinicius.mamore@gmail.com>
2020-05-21 19:03:53 -0400

    criacao de projeto para capitulo 6

---
+++ 6-qualidade-codigo-do-teste/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace qualidade_codigo_do_teste
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
