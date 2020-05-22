3be05cea0cfa47537a2bae4af33b2159ea8a4f78
Vinicius Mamoré <vinicius.mamore@gmail.com>
2020-05-22 16:19:46 -0400

    criacao de primeiro metodo de teste

--- 8-tdd-e-o-acoplamento/UnitTest1.cs
+++ 8-tdd-e-o-acoplamento/UnitTest1.cs
@@ -11,4 +11,41 @@ namespace tdd_e_o_acoplamento
 
         }
     }
+
+    public class Pedido {
+        public string Cliente { get; set; }
+        public decimal ValorTotal { get; set; }
+        public int QuantidadeItens { get; set; }
+
+        public Pedido(string cliente, decimal valorTotal, int quantidadeItens) {
+            this.Cliente = cliente;
+            this.ValorTotal = valorTotal;   
+            this.QuantidadeItens = quantidadeItens;
+        }
+    }
+
+    public class NotaFiscal {
+        public string Cliente { get; set; }
+        public decimal Valor { get; set; }
+        public DateTime Data { get; set; }
+
+        public NotaFiscal(string cliente, decimal valor, DateTime data){
+            this.Cliente = cliente;
+            this.Valor=valor;
+            this.Data = data;
+        }
+    }
+
+    public class SAP
+    {
+        public void Envia(NotaFiscal nf){
+
+        }
+    }
+
+    public class NFDao {
+        public void Persiste(NotaFiscal nf){
+            
+        }
+    }
 }
