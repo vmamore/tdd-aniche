af9552df08b93c376c21af24898153ffb602f1df
unknown <vinicius.mamore@gmail.com>
2020-05-21 23:23:17 -0400

    adicionado calculo de salario para dba

--- 7-tdd-e-a-coesao/CalculadoraDeSalarioTestes.cs
+++ 7-tdd-e-a-coesao/CalculadoraDeSalarioTestes.cs
@@ -17,7 +17,8 @@ namespace tdd_e_a_coesao
         }
 
         [Fact]
-        public void DeveCalcularSalarioParaDesenvolvedorComSalarioAcimaDoLimite(){
+        public void DeveCalcularSalarioParaDesenvolvedorComSalarioAcimaDoLimite()
+        {
             CalculadoraDeSalario calculadora = new CalculadoraDeSalario();
             Funcionario desenvolvedor = new Funcionario("Vinicius", 4000.0m, Cargo.DESENVOLVEDOR);
 
@@ -27,7 +28,8 @@ namespace tdd_e_a_coesao
         }
 
         [Fact]
-        public void DeveCalcularSalarioParaDBAsComSalarioAbaixoDoLimite(){
+        public void DeveCalcularSalarioParaDBAsComSalarioAbaixoDoLimite()
+        {
             CalculadoraDeSalario calculadora = new CalculadoraDeSalario();
             Funcionario desenvolvedor = new Funcionario("Vinicius", 1500.0m, Cargo.DBA);
 
@@ -37,7 +39,8 @@ namespace tdd_e_a_coesao
         }
 
         [Fact]
-        public void DeveCalcularSalarioParaDBAsComSalarioAcimaDoLimite(){
+        public void DeveCalcularSalarioParaDBAsComSalarioAcimaDoLimite()
+        {
             CalculadoraDeSalario calculadora = new CalculadoraDeSalario();
             Funcionario desenvolvedor = new Funcionario("Vinicius", 4500.0m, Cargo.DBA);
 
@@ -47,30 +50,49 @@ namespace tdd_e_a_coesao
         }
     }
 
-    public enum Cargo {
+    public enum Cargo
+    {
         DESENVOLVEDOR,
         DBA,
         TESTADOR
     }
 
-    public class Funcionario {
+    public class Funcionario
+    {
         public string Nome { get; private set; }
         public decimal Salario { get; private set; }
         public Cargo Cargo { get; private set; }
 
-        public Funcionario(string nome, decimal salario, Cargo cargo){
+        public Funcionario(string nome, decimal salario, Cargo cargo)
+        {
             this.Nome = nome;
-            this.Salario =salario;
+            this.Salario = salario;
             this.Cargo = cargo;
         }
     }
 
-    public class CalculadoraDeSalario {
-        public decimal CalculaSalario(Funcionario funcionario){
-            if(funcionario.Salario > 3000){
-                return funcionario.Salario * 0.8m;
+    public class CalculadoraDeSalario
+    {
+        public decimal CalculaSalario(Funcionario funcionario)
+        {
+            if (funcionario.Cargo == Cargo.DESENVOLVEDOR)
+            {
+                if (funcionario.Salario > 3000)
+                {
+                    return funcionario.Salario * 0.8m;
+                }
+                return funcionario.Salario * 0.9m;
             }
-            return funcionario.Salario * 0.9m;
+            else if (funcionario.Cargo == Cargo.DBA)
+            {
+                if (funcionario.Salario < 2500)
+                {
+                    return funcionario.Salario * 0.85m;
+                }
+                return funcionario.Salario * 0.75m;
+            }
+
+            throw new ArgumentException("Funcionário inválido!");
         }
     }
 }
