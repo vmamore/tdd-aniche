6fc91f26dfed578e01a187c0882f2ea33c7f68ce
Vinicius Mamoré <vinicius.mamore@gmail.com>
2020-05-22 18:56:41 -0400

    alteracoes para teste passar

--- 9-tdd-e-o-encapsulamento/ProcessadorDeBoletosTest.cs
+++ 9-tdd-e-o-encapsulamento/ProcessadorDeBoletosTest.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using Xunit;
 
 namespace tdd_e_o_encapsulamento
@@ -10,15 +12,67 @@ namespace tdd_e_o_encapsulamento
         {
             ProcessadorDeBoletos processador = new ProcessadorDeBoletos();
 
-            Fatura fatura = new Fatura("Cliente", 150.0);
-            Boleto b1 = new Boleto(150.0);
+            Fatura fatura = new Fatura("Cliente", 150.0m);
+            Boleto b1 = new Boleto(150.0m);
 
             List<Boleto> boletos = new List<Boleto> { b1 };
 
             processador.Processa(boletos, fatura);
 
             Assert.Equal(1, fatura.Pagamentos.Count());
-            Assert.Equal(150.0, fatura.Pagamentos.First().Valor);
+            Assert.Equal(150.0m, fatura.Pagamentos.First().Valor);
+        }
+
+        public class ProcessadorDeBoletos
+        {
+            public void Processa(List<Boleto> boletos, Fatura fatura)
+            {
+                Boleto boleto = boletos.First();
+                Pagamento pagamento = new Pagamento(boleto.Valor, MeioDePagamento.BOLETO);
+
+                fatura.Pagamentos.Add(pagamento);
+            }
+        }
+
+        public class Fatura
+        {
+            public ICollection<Pagamento> Pagamentos { get; }
+            public string Cliente { get; }
+            public decimal Valor { get; }
+
+            public Fatura(string cliente, decimal valor)
+            {
+                this.Cliente = cliente;
+                this.Valor = valor;
+                this.Pagamentos = new List<Pagamento>();
+            }
+        }
+
+        public class Boleto
+        {
+            public decimal Valor { get; }
+
+            public Boleto(decimal valor)
+            {
+                this.Valor = valor;
+            }
+        }
+
+        public enum MeioDePagamento
+        {
+            BOLETO
+        }
+
+        public class Pagamento
+        {
+            public decimal Valor { get; set; }
+            public MeioDePagamento MeioDePagamento { get; set; }
+
+            public Pagamento(decimal valor, MeioDePagamento meioDePagamento)
+            {
+                this.Valor = valor;
+                this.MeioDePagamento = meioDePagamento;
+            }
         }
     }
 }
