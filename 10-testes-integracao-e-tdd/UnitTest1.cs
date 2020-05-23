5e2e6f857def5691ccb1f7feb0d59029b1a9eddd
Vinicius Mamoré <vinicius.mamore@gmail.com>
2020-05-23 11:01:58 -0400

    bom insight sobre execucao de testes de infraestrutura

--- 10-testes-integracao-e-tdd/UnitTest1.cs
+++ 10-testes-integracao-e-tdd/UnitTest1.cs
@@ -5,6 +5,9 @@ namespace testes_integracao_e_tdd
 {
     // Classes que lidam com infraestrutura devem ser testadas com a
     // infraestrutura real que lidarão. É trabalhoso, mas necessario.
+
+    // Cada teste precisa ter o banco de dados limpo para que os dados antigos
+    // não influenciem no resultado
     public class UnitTest1
     {
         [Fact]
