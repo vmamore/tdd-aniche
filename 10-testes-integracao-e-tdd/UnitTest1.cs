4f258da6e258a52cfbf63a654686ebc4de43e2f3
Vinicius Mamoré <vinicius.mamore@gmail.com>
2020-05-23 11:01:05 -0400

    bom insight sobre testes de infraestrutura

--- 10-testes-integracao-e-tdd/UnitTest1.cs
+++ 10-testes-integracao-e-tdd/UnitTest1.cs
@@ -3,6 +3,8 @@ using Xunit;
 
 namespace testes_integracao_e_tdd
 {
+    // Classes que lidam com infraestrutura devem ser testadas com a
+    // infraestrutura real que lidarão. É trabalhoso, mas necessario.
     public class UnitTest1
     {
         [Fact]
