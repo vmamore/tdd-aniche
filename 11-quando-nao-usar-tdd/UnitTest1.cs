5ce25826a80843e7208c4818bf32f6df8d1ce1ba
Vinicius Mamoré <vinicius.mamore@gmail.com>
2020-05-23 11:22:11 -0400

    bom insight sobre experiencia ditar quando usar tdd

--- 11-quando-nao-usar-tdd/UnitTest1.cs
+++ 11-quando-nao-usar-tdd/UnitTest1.cs
@@ -5,6 +5,9 @@ namespace quando_nao_usar_tdd
 {
     // A ideia não é usar TDD porque a prática está em alta no mercado
     // mas sim pelo benefício que ele agrega ao processo
+
+    // Deixe sua experiência te guiar também na escolha de usar ou não TDD.
+    // Lembre-se que no fim das contas, o que precisamos é de feedback sobre o que estamos fazendo
     public class UnitTest1
     {
         [Fact]
