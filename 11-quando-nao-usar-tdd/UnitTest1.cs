ab940332c1de43e1b2d722f3b2b1dfae3d44be56
Vinicius Mamoré <vinicius.mamore@gmail.com>
2020-05-23 11:55:16 -0400

    bom insight sobre testes em sistemas legados

--- 11-quando-nao-usar-tdd/UnitTest1.cs
+++ 11-quando-nao-usar-tdd/UnitTest1.cs
@@ -12,6 +12,18 @@ namespace quando_nao_usar_tdd
     // Cuidado na hora de julgar se um código merece ou não ser testado.
     // Na dúvida, teste.
     // Lembre-se que todo código é considerado culpado até que se prove inocente.
+
+    // Refatoração em sistemas legados:
+    // 1 - Comece testando da maneira que conseguir
+    // - 1.1 - Se conseguir teste unidade, excelente.
+    // - 1.2 - Se não, subir de nível até conseguir escrever um teste.
+    // - - 1.2.1 - Em aplicações legadas web, o dev pode começar com testes de sistema, para garantir o comportamento do sistema.
+    // 2 - Na refatoração, o dev deve criar classes menores e mais coesas e já escrever testes de unidade para elas.
+    // 3 - Durante a reescrita, pode ser que o dev sinta a necessidade de escrever testes "feios".
+    // - 3.1 - Em sistemas legados, não tenha medo de escrever código de teste que será jogado fora depois.
+    //         Se fizermos uma analogia, imagine esses códigos sejam como os andaimes utilizados durante a obra.
+    //         Eles não fazem parte do produto final, estão lá apenas para ajudar no desenvolvimento.
+    // - 3.2 - O padrão "Adapter" pode ajudar o dev a conectar código bem escrito com o legado que deve ser mantido.
     public class UnitTest1
     {
         [Fact]
